932c377aa41175024207c2a27c5da99d3557c1a5
Oborotov Iliya <obronat5@gmail.com>
2020-03-10 18:24:54 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-import socket
 import sys
+import socket
 import select
 import errno
 
@@ -7,43 +7,54 @@ HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 8000
 
+class Client:
+    def __init__(self, client_socket, username):
+        self.client_socket = client_socket
+        self.username = my_username.encode('utf-8')
+        self.username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+        self.client_socket.send(self.username_header + self.username)
+    
+    def send_message(self, message):
+        if not message:
+            return
+        message = message.encode('utf-8')
+        message = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8') + message
+        self.client_socket.send(message)
+    
+    def recieve_message(self):
+        username_header = self.client_socket.recv(HEADER_LENGTH)
+        if not len(username_header):
+            print("Connection closed by the server")
+            sys.exit()
+
+        username_length = int(username_header.decode('utf-8'))
+        username = self.client_socket.recv(username_length).decode('utf-8')
+
+        message_header = self.client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode('utf-8'))
+        message = self.client_socket.recv(message_length).decode('utf-8')
+
+        print(f"{username} > {message}")
+
 my_username = input("Username: ")
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
-client_socket.setblocking(False) # recv() win't be blocked
+client_socket.setblocking(False) # recv() won't be blocked
 
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-
-client_socket.send(username_header + username)
+client = Client(client_socket, my_username)
 
 while True:
-    message = input(f"{my_username} > ")
-
-    if message:
-        message = message.encode('utf-8')
-        message = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8') + message
-        client_socket.send(message)
+    client.send_message(input(f"{my_username} > "))
     try:
         while True:
-            username_header = client_socket.recv(HEADER_LENGTH)
-            if not len(username_header):
-                print("Connection closed by the server")
-                sys.exit()
-            username_length = int(username_header.decode('utf-8')) # encode?
-            username = client_socket.recv(username_length).decode('utf-8')
-
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8'))
-            message = client_socket.recv(message_length).decode('utf-8')
-
-            print(f"{username} > {message}")
+            client.recieve_message()
     except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print("Reading error", str(e))
+            print("Reading error ", str(e))
             sys.exit()
         continue
+
     except Exception as e:
-        print("General Error", str(e))
+        print("General Error ", str(e))
         sys.exit()
